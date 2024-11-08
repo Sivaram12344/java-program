b4857404dc26f3359921ce93c74ae72838652d6d
Sivaram12344 <sivarammanchala@gmail.com>
2024-11-08 10:54:00 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,12 @@
+class Emp
+{
+public static void main(String args[])
+{
+String Emp_name="SHIVA RAM";
+int Emp_number=76;
+float Emp_salary=674.50F;
+System.out.println("The Given Employee Name is"+Emp_name);
+System.out.println("The Given Employee Number is"+Emp_number);
+System.out.println("The Given Employee Salary is"+Emp_salary);
+}
+}
---
+++ First.java
@@ -0,0 +1,12 @@
+class Emp
+{
+public static void main(String args[])
+{
+String Emp-name="SHIVA RAM";
+int Emp-number=76;
+float Emp-salary=674.50F;
+System.out.println("The Given Employee Name is"+Emp-name);
+System.out.println("The Given Employee Number is"+Emp-number);
+System.out.println("The Given Employee Salary is"+Emp-salary);
+}
+}
